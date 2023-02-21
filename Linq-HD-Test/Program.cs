7a6e682f0ae855b672b222d2506ccf28d7769873
Hans Danielsson <Hans.Ro.Danielsson@gmail.com>
2023-02-21 16:28:02 +0100

    Add project files.

---
+++ Linq-HD-Test/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] cities = { "Boston", "Los Engeles", "Seattle", "London", "Hyderabad" };
+
+Console.WriteLine(cities.Length);
+IEnumerable<string> fileredList = cities.Where((string s) =>
+{
+  return s.ToLower().StartsWith("l");
+});
+
+Console.WriteLine("Nu kommer listan:");
+foreach (var Item in fileredList)
+{
+  Console.WriteLine(Item);
+}
+Console.WriteLine(fileredList.Count());
